27971fec170bf0d00e9e6a06f53d0280ef1f84e7
Cristian Metz <c.metz@outlook.com>
2022-05-03 15:13:38 -0400

    completed challenge 1

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,16 +1,52 @@
 // Values and Varriables
-let country = 'USA';
+//1
+let country = 'The United States of America';
 let continent = 'North America';
 let population = 332403650;
-
+//2
 console.log(country, continent, population);
 
 //Data Types
+//1
 const isIsland = false;
 let language = null;
-
-console.log(isIsland,population,country,language);
+//2
+console.log(typeof isIsland,typeof population,typeof country,typeof language);
 
 //let, const and var
-language = 'US-EN'
+//1
+language = 'English'
 console.log(language);
+
+//Basic Operators
+//1
+const halfPopulation = population / 2;
+console.log('If the country split in half the population would be '+halfPopulation);
+//2
+population += 1; 
+console.log(population);
+//3
+const finland = 6000000;
+console.log(population > finland);
+//4
+const averageCountryPopulation = 33000000;
+console.log(population < averageCountryPopulation);
+//5
+const description = country + " is in " + continent + ", and its " + population + ' million people speak ' + language;
+console.log(description);
+
+//Coding Challenge #1
+//1
+const massJohn = 92;
+const heightJohn = 1.95;
+const massMark = 78;
+const heightMark = 1.69;
+//2
+const johnBMI = massJohn / (heightJohn * heightJohn);
+console.log(johnBMI);
+const markBMI = massMark / heightMark ** 2;
+console.log(markBMI);
+//3
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
