453f5429bcb211ada3eaefdae1c0c4db960cd740
Cristian Metz <c.metz@outlook.com>
2022-05-03 13:01:48 -0400

    created assignment file

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,16 @@
+// Values and Varriables
+let country = 'USA';
+let continent = 'North America';
+let population = 332403650;
+
+console.log(country, continent, population);
+
+//Data Types
+const isIsland = false;
+let language = null;
+
+console.log(isIsland,population,country,language);
+
+//let, const and var
+language = 'US-EN'
+console.log(language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,7 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN');
-
 console.log(40+8+23-10);
+
+console.log('23');
+
+let firstName = 'Cristian';
+console.log(firstName);
