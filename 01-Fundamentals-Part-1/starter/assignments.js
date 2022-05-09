d3e044cf96e852d94ea6dcad061a9b0fdb00b93e
Cristian Metz <c.metz@outlook.com>
2022-05-09 12:32:32 -0400

    refactor

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -214,31 +214,31 @@ arabic: '5th most spoken language'
 for all other simply log 'Great language too :D'
 */
 
-let spokenLanguage = 'english'
+// let spokenLanguage = 'english'
 
-switch (spokenLanguage) {
-  case 'chinese':
-  case 'mandarin':
-    console.log('Most number of native speakers!');
-    break;
+// switch (spokenLanguage) {
+//   case 'chinese':
+//   case 'mandarin':
+//     console.log('Most number of native speakers!');
+//     break;
   
-  case 'spanish':
-    console.log('2nd place in number of native speakers!');
-    break;
+//   case 'spanish':
+//     console.log('2nd place in number of native speakers!');
+//     break;
   
-  case 'english':
-    console.log('3rd place');
-    break;
+//   case 'english':
+//     console.log('3rd place');
+//     break;
   
-  case 'hindi':
-    console.log('Number 4');
-    break;
+//   case 'hindi':
+//     console.log('Number 4');
+//     break;
 
-  case 'arabic':
-    console.log('5th most spoken language');
-    break;
+//   case 'arabic':
+//     console.log('5th most spoken language');
+//     break;
 
-  default:
-    console.log('Great language too :D');
-}
+//   default:
+//     console.log('Great language too :D');
+// }
 
