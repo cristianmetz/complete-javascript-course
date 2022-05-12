09bfe46e3e01b2cebe2e9a14e50a6341aa195f25
Cristian Metz <c.metz@outlook.com>
2022-05-12 18:28:56 -0400

    finished coding challenge 2

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -188,17 +188,23 @@ Your tasks:
 Test data: 125, 555 and 44
 
 */
+//1 //2
 const bill = [125, 555, 44];
 
 function calcTip(bill) {
   if (bill >= 50 && bill <= 300) {
     let tip = bill * .15;
-    console.log(tip);
+    // console.log(tip);
+    return tip;
   } else {
     let tip = bill * .20;
-    console.log(tip);
+    // console.log(tip);
+    return tip;
   }
 }
-
-const tips = [calcTip(bill[0], calcTip(bill[1]), calcTip(bill[2]))];
+//3
+const tips = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
 console.log(tips);
+//4 BONUS
+const total = [tips[0]+bill[0], tips[1]+bill[1], tips[2]+bill[2]];
+console.log(total);
