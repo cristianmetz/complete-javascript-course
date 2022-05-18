f809a6749a7a48ff18c0140985c0ba966c059c9d
Cristian Metz <c.metz@outlook.com>
2022-05-17 20:47:31 -0400

    completed challenge 3

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -294,6 +294,39 @@ method on both objects). Store the BMI value to a property, and also return it
 from the method
 3. Log to the console who has the higher BMI, together with the full name and the
 respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+
 Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
 tall.
  */
+
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function() {
+//         this.bmi = Math.trunc(this.mass / this.height ** 2)
+//         return this.bmi
+//     }
+// }
+
+// console.log(mark.calcBMI());
+
+// const john = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         this.bmi = Math.trunc(this.mass / this.height ** 2)
+//         return this.bmi
+//     }
+// }
+
+// console.log(john.calcBMI());
+
+
+//     if (mark.bmi > john.bmi) {
+//         console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`);
+//     } else if (john.bmi > mark.bmi) {
+//         console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
+//     }
+
