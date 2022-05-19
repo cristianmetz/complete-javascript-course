b4e72aae9f38cafb58be87f18e911f5f7df50236
Cristian Metz <c.metz@outlook.com>
2022-05-18 21:55:54 -0400

    added lecture assignment

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -338,3 +338,14 @@ Use a for loop to simulate the 50 people voting, by logging a string like this t
 // for( let vote = 1; vote <=50; vote++) {
 //     console.log(`Voter number ${vote} is currently voting`);
 // }
+
+/*
+LECTURE: Looping Arrays, Breaking and Continuing
+1. Let's bring back the 'populations' array from a previous assignment
+2. Use a for loop to compute an array called 'percentages2' containing the
+percentages of the world population for the 4 population values. Use the
+function 'percentageOfWorld1' that you created earlier
+3. Confirm that 'percentages2' contains exactly the same values as the
+'percentages' array that we created manually in the previous assignment,
+and reflect on how much better this solution is
+ */
