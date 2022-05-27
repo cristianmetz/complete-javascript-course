935a75d17e9c5fdceeffbd466bad1e200ea77a58
Cristian Metz <c.metz@outlook.com>
2022-05-26 20:45:00 -0400

    added lecture exercise

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -396,3 +396,10 @@ worry if it's too difficult for you! But you can still try to figure this out an
 //         console.log(`------------------------------------------`)
 //     }
 // }
+/*
+LECTURE: The while Loop
+1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+but this time using a while loop (call the array 'percentages3')
+2. Reflect on what solution you like better for this task: the for loop or the while
+loop?
+ */
